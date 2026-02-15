8a4aa170ac8f9197d9d37444f461f5a063b7aac3
Pratikesh <pratikeshb9@gmail.com>
2026-02-15 22:26:19 +0530

    booking request dto

---
+++ src/main/java/com/pratikesh/airBnbApp/dto/BookingRequestDTO.java
@@ -0,0 +1,14 @@
+package com.pratikesh.airBnbApp.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+
+public class BookingRequestDTO {
+    private Long hotelId;
+    private Long roomId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private Integer roomsCount;
+}
